62244b475010b86681b10bb52f9474f6c365fbcd
tangxuke <295659187@qq.com>
2018-07-14 10:53:00 +0800

    111

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User=require('./../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,38 @@ router.get('/', function(req, res, next) {
 
 router.get('/about',function(req,res,next){
   res.send({'test':'hello'});
+})
+
+var response={
+  success:false,
+  message:'',
+  result:[]
+}
+
+router.post('/add',function(req,res,next){
+  
+  User.findOne({'username':req.body.username})
+    .then((doc)=>{
+      if(doc){
+        response.success=false;
+        response.message='用户已存在！'
+        res.json(response);
+      }else{
+        User.create(req.body).then((value)=>{
+          response.success=true;
+          response.message=''
+        }).catch((err1)=>{
+          response.success=false;
+          response.message=err1.message;
+        }).then(()=>{
+          res.json(response);
+        })
+      }
+    }).catch((err)=>{
+      response.success=false;
+      response.message=err.message;
+      res.json(response);
+    })
 })
 
 module.exports = router;
