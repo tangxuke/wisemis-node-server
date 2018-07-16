fede5af515eb6ced647093e3d46b8b7456cfff25
tangxuke <295659187@qq.com>
2018-07-16 13:28:44 +0800

    111

--- models/custom-model.js
+++ models/custom-model.js
@@ -1,21 +1,33 @@
 var mongoose=require('mongoose')
 var TheModel=require('./model')
 
-module.exports=function(modelName){
-        var theSchama=new mongoose.Schema();
-        //读取模板
-        TheModel.findOne({'name':modelName})
-        .then((doc)=>{
-            if(doc){
-                //生成架构
-                theSchama.add(doc.schama);
 
-                //resolve(mongoose.model(doc.name,theSchama,doc.collname));
-                return mongoose.model(doc.name,doc.schama,doc.collname);
-            }else{
-                return new Error('模板不存在')
+module.exports=function(modelName){
+        var p=new Promise((resolve,reject)=>{
+
+            if(mongoose.modelNames().findIndex((item)=>{
+                return item==modelName
+            })>-1)
+            {
+                resolve(mongoose.model(modelName))
+                return;
             }
-        }).catch((err)=>{
-            return err
-        });
+
+            //读取模板
+            TheModel.findOne({'name':modelName})
+            .then((doc)=>{
+                if(doc){
+                    //生成架构
+                    var theSchama=mongoose.Schema(doc.schama)
+
+                    resolve(mongoose.model(doc.name,theSchama,doc.collname));
+                }else{
+                    reject(new Error('模板不存在'))
+                }
+            }).catch((err)=>{
+                reject(new Error('0'+err.message))
+            });
+        })
+
+        return p;
 }
--- routes/custom-model.js
+++ routes/custom-model.js
@@ -5,32 +5,43 @@ var router=express.Router();
 
 var GetModel=require('./../models/custom-model');
 
+var CustomModel;
 //新建记录
 router.post('/:model/new',(req,res,next)=>{
-    var CustomModel=GetModel(req.params.model);
+    var p=GetModel(req.params.model);
 
-    CustomModel.create(req.body)
-    .then((doc)=>{
-            res.json({
-                success:true,
-                message:'',
-                result:doc
+    p.then((value)=>{
+        value.create(req.body)
+            .then((doc)=>{
+                res.json({
+                    success:true,
+                    message:'',
+                    result:doc
+                })
             })
-        })
-        .catch((error)=>{
-            res.json({
-                success:false,
-                message:error.message,
-                result:null
+            .catch((err)=>{
+                res.json({
+                    success:false,
+                    message:err.message,
+                    result:null
+                })
             })
+    })
+    .catch((error)=>{
+        res.json({
+            success:false,
+            message:error.message,
+            result:null
         })
+    })
+    
 })
 
 //读取数据
 router.get('/:model',(req,res,next)=>{
-    var CustomModel=GetModel(req.params.model);
-    console.log(CustomModel)
-        CustomModel.find().then((docs)=>{
+    var p=GetModel(req.params.model);
+    p.then((value)=>{
+        value.find().then((docs)=>{
             res.json({
                 success:true,
                 message:'',
@@ -40,10 +51,18 @@ router.get('/:model',(req,res,next)=>{
         .catch((error)=>{
             res.json({
                 success:false,
-                message:error.message,
+                message:'1'+error.message,
                 result:null
             })
         })
+    }).catch((err)=>{
+        res.json({
+                success:false,
+                message:'2'+err.message,
+                result:null
+            })
+    })
+        
 })
 
 module.exports=router
