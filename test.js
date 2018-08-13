83fd340469dda817280019e52e4fc809a6f12880
tangxuke <295659187@qq.com>
2018-08-13 23:44:52 +0800

    111

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
-function f(){
-    console.log('hello')
-}
+var mysql=require('mysql');
 
-setTimeout(f,2000)
-setInterval(f,2000)
+var connection=mysql.createConnection({
+    host:'gz-cdb-0jo34sqt.sql.tencentcdb.com',
+    port:62178,
+    user:'root',
+    password:'hlh***TXK0921',
+    database:'test'
+});
+
+connection.connect((err)=>{
+    if(err)
+        console.log(err.message);
+    else
+    {
+        console.log('连接MySQL成功！');
+        connection.query('select * from customer',function(error,results,fields){
+            if(error)
+                console.log(error.message);
+            else{
+                console.log(fields);
+                console.log(results);
+            }
+            connection.end();
+        })
+        
+    }
+})
