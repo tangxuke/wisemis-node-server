55b2000cecd75435cea0cb3b15d62a95230de18c
tangxuke <295659187@qq.com>
2018-08-29 16:52:55 +0800

    111

--- models/model/action/data-for-page.js
+++ models/model/action/data-for-page.js
@@ -5,20 +5,9 @@ var Model=require('../index')
  * @param {Model} model 模型对象
  * @param {object} data 参数对象
  */
-module.exports=function(model,data){
-    return new Promise(function(resolve,reject){
+module.exports=async function(model,data){
         var pageSize = data['page-size'] || 10; //默认一页10条记录
         var currentPage = data['current-page'] || 1;    //当前页，默认1
-        var total=GetTotel(model);
-        console.log(total)
-        resolve(1);
-    })
-}
-
-/**
- * 获取记录总数（同步）
- * @param {Model} model 模型对象
- */
-async function GetTotel(model){
-    return await mysql('select count(*) as total from '+model.TableName,[],model.Database);
+        var totalPromise=await mysql('select count(*) as total from '+model.TableName,[],model.Database);
+        
 }
