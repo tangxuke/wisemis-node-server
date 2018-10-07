cbde2df6d87a404fb1ff43f309ea88f17bd68908
tangxuke <295659187@qq.com>
2018-10-08 00:51:14 +0800

    111

--- models/model/field/from-row/index.js
+++ models/model/field/from-row/index.js
@@ -3,7 +3,7 @@ var Field=require('..');
 /**
  * 从数据行创建字段
  * @param {Field} field 字段对象
- * @param {{field_name:string,field_title:string,field_type:string,field_length:number,grid_column_width:number,form_col_span:number,control_type:string,icon:string,is_insert:boolean,is_update:boolean,is_key:boolean,is_not_null:boolean,show_in_form:boolean,show_in_grid:boolean,orderid:number,reg_expression:string,system:boolean,default_value:string,tool_tip:string,page:string,data_source_type:string,data_source:string,data_source_sql_return_field:string,data_srouce_sql_database:string,control_height:number}} row 行数据
+ * @param {{field_name:string,field_title:string,field_type:string,field_length:number,grid_column_width:number,form_col_span:number,control_type:string,icon:string,is_insert:boolean,is_update:boolean,is_key:boolean,search_field:boolean,is_not_null:boolean,show_in_form:boolean,show_in_grid:boolean,orderid:number,reg_expression:string,system:boolean,default_value:string,tool_tip:string,page:string,data_source_type:string,data_source:string,data_source_sql_return_field:string,data_srouce_sql_database:string,control_height:number}} row 行数据
  */
 function SetField(field,row){
     field.setName(row.field_name)
@@ -16,6 +16,7 @@ function SetField(field,row){
     .setIcon(row.icon)
     .setIsInsert(row.is_insert)
     .setIsUpdate(row.is_update)
+    .setSearchField(row.search_field)
     .setShowInForm(row.show_in_form)
     .setShowInGrid(row.show_in_grid)
     .setSystem(row.system)
