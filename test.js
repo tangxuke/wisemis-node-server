ca885896f3e2c24e17717019e2c860f360f812d9
tangxuke <295659187@qq.com>
2018-10-16 16:59:26 +0800

    111

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
-var s='a{{new Date()}}cd';
-var a=s.indexOf('{{');
-var b=s.indexOf('}}',a);
-
-var e=s.substr(a+2,b-a-2);
-var s1=s.substr(0,a);
-
-var s2=s.substr(b+2);
-
-var o=s1+eval(e)+s2;
-console.log(o);
+const XmlReader = require('xml-reader');
+const reader = XmlReader.create();
+const xml =
+    `<Button type="success">{{test}}</Button>`;
+ 
+reader.on('done', data => console.log(data.attributes));
+reader.parse(xml);
